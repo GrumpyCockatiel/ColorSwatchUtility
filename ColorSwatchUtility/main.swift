d2f3a6d5455ccc0832d810d886ec7b249717af28
Grumpy Cockatiel <tguillory@gmail.com>
2018-08-01 17:44:40 -0500

    some comments

--- ColorSwatchUtility/main.swift
+++ ColorSwatchUtility/main.swift
@@ -8,20 +8,25 @@
 
 import Foundation
 
+// create some colors
 let black:ASEColor = ASEColor("#000000", name:"Black");
 let white:ASEColor = ASEColor("#FFFFFF", name:"White");
 let red:ASEColor = ASEColor("#FF0000", name:"Red");
 let green:ASEColor = ASEColor("#00FF00", name:"Green");
 let blue:ASEColor = ASEColor("#0000FF", name:"Blue");
 
+// create a group
 let group:ASEGroup = ASEGroup("Primaries");
 group.colors.append(red);
 group.colors.append(green);
 group.colors.append(blue);
 
+// create the manger and add everything
 let io:ASEManager = ASEManager("Test");
 io.colors = [black,white];
 io.groups = [group];
+
+// write the file
 io.write();
 
 print("Wrote \(io.colors.count) colors.");
